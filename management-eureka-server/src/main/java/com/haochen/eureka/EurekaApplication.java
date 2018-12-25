0c731ad1196edad6e33205839529b69d5ade4808
YangChao <yangchao@babysky.com.cn>
2018-12-25 10:21:19 +0800

    删除一些冗余的类和文件

--- management-eureka-server/src/main/java/com/haochen/eureka/EurekaApplication.java
+++ management-eureka-server/src/main/java/com/haochen/eureka/EurekaApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
